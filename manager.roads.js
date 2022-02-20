9308b84aafae7b41148260bac88a811f7256dc61
William Bruneau <william.bruneau@epfedu.fr>
2022-02-20 16:20:50 +0100

    Ajout manager de roads (oups 2)

--- manager.roads.js
+++ manager.roads.js
@@ -5,7 +5,7 @@ let roadManager = {
         let sources = room.find(FIND_SOURCES)
         let controllers = room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_CONTROLLER } })
 
-        for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++)
+        for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
             for (let source_index = 0; source_index < sources.length; source_index++)
             {
                 let paths = spawns[spawn_index].pos.findPathTo(sources[source_index].pos)
@@ -14,7 +14,9 @@ let roadManager = {
                     room.createConstructionSite(paths[path_index].x,paths[path_index].y, STRUCTURE_ROAD)
                 }
             }
+        }
 
+        for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
             for (let controller_index = 0; controller_index < controllers.length; controller_index++)
             {
                 let paths = spawns[spawn_index].pos.findPathTo(controllers[controller_index].pos)
@@ -24,6 +26,7 @@ let roadManager = {
                 }
             }
         }
+    }
 };
 
 module.exports = roadManager;
