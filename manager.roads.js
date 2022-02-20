b242e45146fa5e9b6f4821f17cd96854ecbc6fc8
William Bruneau <william.bruneau@epfedu.fr>
2022-02-20 17:10:19 +0100

    manager.roads: chemin sans contournement de route

--- manager.roads.js
+++ manager.roads.js
@@ -8,8 +8,9 @@ let roadManager = {
         for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
             for (let source_index = 0; source_index < sources.length; source_index++)
             {
-                let paths = room.findPath(spawns[spawn_index].pos, sources[source_index].pos, { ignoreCreeps: true })
-                for (let path_index = 0; path_index < paths.length; path_index++) 
+                let paths = room.findPath(spawns[spawn_index].pos, sources[source_index].pos, { ignoreCreeps: true, ignoreRoads: true })
+                // lenght-1 to avoid building road on source
+                for (let path_index = 0; path_index < paths.length - 1; path_index++) 
                 {
                     room.createConstructionSite(paths[path_index].x,paths[path_index].y, STRUCTURE_ROAD)
                 }
@@ -19,8 +20,9 @@ let roadManager = {
         for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
             for (let controller_index = 0; controller_index < controllers.length; controller_index++)
             {
-                let paths = room.findPath(spawns[spawn_index].pos, controllers[controller_index].pos, { ignoreCreeps: true })
-                for (let path_index = 0; path_index < paths.length; path_index++) 
+                let paths = room.findPath(spawns[spawn_index].pos, controllers[controller_index].pos, { ignoreCreeps: true, ignoreRoads: true })
+                // lenght-1 to avoid building road on controller
+                for (let path_index = 0; path_index < paths.length - 1; path_index++) 
                 {
                     room.createConstructionSite(paths[path_index].x,paths[path_index].y, STRUCTURE_ROAD)
                 }
