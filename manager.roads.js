307b8313a3f1fddf58d5969f0489d4f6458facee
William Bruneau <william.bruneau@epfedu.fr>
2022-02-20 18:15:05 +0100

    MODIFIE roads: routes entre sources et controllers

--- manager.roads.js
+++ manager.roads.js
@@ -5,11 +5,19 @@ let roadManager = {
         let sources = room.find(FIND_SOURCES)
         let controllers = room.find(FIND_MY_STRUCTURES, {filter: { structureType: STRUCTURE_CONTROLLER } })
 
-        for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
-            for (let source_index = 0; source_index < sources.length; source_index++)
+        buildRoad(spawns, sources)
+        buildRoad(spawns, controllers)
+        buildRoad(sources, controllers)
+    },
+
+    /** @param {Array} departures **/
+    /** @param {Array} arrivals **/
+    buildRoad: function(departures, arrivals) {
+        for (let departures_index = 0; departures_index < departuress.length; departures_index++) {
+            for (let arrival_index = 0; arrival_index < arrivals.length; arrival_index++)
             {
-                let paths = room.findPath(spawns[spawn_index].pos, sources[source_index].pos, { ignoreCreeps: true, ignoreRoads: true })
-                // lenght-1 to avoid building road on source
+                let paths = room.findPath(departures[departures_index].pos, arrivals[arrival_index].pos, { ignoreCreeps: true, ignoreRoads: true })
+                // lenght-1 to avoid building road on arrival
                 for (let path_index = 0; path_index < paths.length - 1; path_index++) 
                 {
                     room.createConstructionSite(paths[path_index].x,paths[path_index].y, STRUCTURE_ROAD)
@@ -17,17 +25,6 @@ let roadManager = {
             }
         }
 
-        for (let spawn_index = 0; spawn_index < spawns.length; spawn_index++) {
-            for (let controller_index = 0; controller_index < controllers.length; controller_index++)
-            {
-                let paths = room.findPath(spawns[spawn_index].pos, controllers[controller_index].pos, { ignoreCreeps: true, ignoreRoads: true })
-                // lenght-1 to avoid building road on controller
-                for (let path_index = 0; path_index < paths.length - 1; path_index++) 
-                {
-                    room.createConstructionSite(paths[path_index].x,paths[path_index].y, STRUCTURE_ROAD)
-                }
-            }
-        }
     }
 };
 
